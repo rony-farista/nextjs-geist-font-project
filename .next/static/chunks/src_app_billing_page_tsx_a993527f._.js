71bee257c9f26d86111194f466f858296cd0870e
rony-farista <ronyfarista@gmail.com>
2025-05-18 01:19:35 +0700

    Add .next/static/chunks/src_app_billing_page_tsx_a993527f._.js 1747505968979

---
+++ .next/static/chunks/src_app_billing_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a96cc14f._.js",
+  "static/chunks/src_330b764c._.js"
+],
+    source: "dynamic"
+});
